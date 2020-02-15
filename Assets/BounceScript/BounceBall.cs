8f8e6a0301a52c5dcd88e748d103c7ac434d77d2
wondae hong <hongwd75@naver.com>
2020-02-15 23:18:08 +0900

    jump with rigidBody

--- Assets/BounceScript/BounceBall.cs
+++ Assets/BounceScript/BounceBall.cs
@@ -4,17 +4,18 @@ using System.Collections;
 public class BounceBall : MonoBehaviour
 {
     public Vector2 startPoint;
-
-    protected bool onGround = false;
+    public float height;
+    public bool onGround = false;
+    public float JumpPower = 3.2f;
     protected Rigidbody2D rigidBody = null;
+
     // Use this for initialization
     void Start()
     {
         onGround = false;
         rigidBody = GetComponent<Rigidbody2D>();
         startPoint = transform.position;
-
-
+        height = transform.localScale.y / GetComponent<SpriteRenderer>().sprite.bounds.size.y+0.2f;
     }
 
 
@@ -36,28 +37,40 @@ public class BounceBall : MonoBehaviour
     }
 
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        if (onGround == true)
+        if (onGround == true && JumpPower != 0.0f)
         {
             onGround = false;
-            rigidBody.AddForce(Vector2.up * 3.2f, ForceMode2D.Impulse);
-        } else
+            rigidBody.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse);
+        }
+        else
         {
             UpdateInput();
         }
-
+    }
+    // Update is called once per frame
+    void Update()
+    {
         if(transform.position.y < 0)
         {
             transform.position = startPoint;
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.layer == 8)
-        {
-            onGround = true;
-        }
-    }
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if(collision.gameObject.layer == 8)
+    //    {
+    //      //  if(Physics2D.Raycast(gameObject.transform.position, Vector2.down, height, 1 << 8) == true)
+    //        { 
+    //            onGround = true;
+    //            var tileAction = collision.gameObject.GetComponent<TileAction>();
+    //            if (tileAction != null)
+    //            {
+    //                Debug.Log("tileAction");
+    //                tileAction.OnGroundBall();
+    //            }
+    //        }
+    //    }
+    //}
 }
---
+++ Assets/BounceScript/TileAction.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileAction : MonoBehaviour
+{
+    public float JumpPower = 3.2f;
+    protected Vector2 RePosition;
+    protected Vector2 moveDirection;
+    protected float jumpSpeed = -0.06f;
+    protected float gravity = 0.6f;
+    protected bool touched = false;
+    public void OnGroundBall()
+    {
+        touched = true;
+        moveDirection.y = jumpSpeed;
+    }
+    // Update is called once per frame
+    private void Start()
+    {
+        RePosition = transform.position;
+    }
+    void Update()
+    {
+        if (touched == true)
+        {
+            moveDirection.y += gravity * Time.deltaTime;
+            transform.Translate(moveDirection);
+
+            if(transform.position.y > RePosition.y)
+            {
+                transform.position = RePosition;
+                touched = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            foreach (ContactPoint2D hitPos in collision.contacts)
+            {
+                if (hitPos.normal.y < 0) // check if its collided on top 
+                {
+                    var Ball = collision.gameObject.GetComponent<BounceBall>();
+                    if (Ball != null)
+                    {
+                        Debug.Log("OnTriggerEnter2D");
+                        OnGroundBall();
+                        Ball.JumpPower = JumpPower;
+                        Ball.onGround = true;
+                    }
+                    break;
+                }
+            }
+        }
+
+    }
+}
