292fda5f35aa72b0d048bce29ddf75711d6268a7
wondae hong <hongwd75@naver.com>
2020-02-18 22:18:41 +0900

    누락된 파일들

---
+++ Assets/BounceScript/StarItem.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarItem : MonoBehaviour
+{
+    protected bool touchedBall = false;
+    protected SpriteRenderer sprite;
+    // Use this for initialization
+    void Start()
+    {
+        sprite = transform.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator DestroyIncomming()
+    {
+        float duration = 0.3f;
+        float startTime = Time.time;
+        Vector2 oPos = transform.position;
+        Vector2 nPos = new Vector2(oPos.x, oPos.y + 1);
+        while (sprite.color.a > 0.0f)
+        {
+            float t = (Time.time - startTime) / duration;
+            sprite.color = new Color(1f, 1f, 1f, Mathf.SmoothStep(1.0f, 0.0f, t));
+            transform.position = new Vector2(oPos.x, Mathf.SmoothStep(oPos.y, nPos.y, t));
+            yield return null;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 9 && touchedBall == false)
+        {
+            touchedBall = true;
+            StartCoroutine("DestroyIncomming");
+            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        
+    }
+}
