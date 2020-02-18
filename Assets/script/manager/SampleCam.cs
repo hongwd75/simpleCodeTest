b3ce832d8e39c05e49d442a4f2a45543a9cd35da
wondae hong <hongwd75@naver.com>
2020-02-18 22:17:16 +0900

    camera test

---
+++ Assets/script/manager/SampleCam.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SampleCam : MonoBehaviour
+{
+    public float left = -0.2F;
+    public float right = 0.2F;
+    public float top = 0.2F;
+    public float bottom = -0.2F;
+    void LateUpdate()
+    {
+        Camera cam = Camera.main;
+        Matrix4x4 m = PerspectiveOffCenter(left, right, bottom, top, cam.nearClipPlane, cam.farClipPlane);
+        cam.projectionMatrix = m;
+    }
+
+    static Matrix4x4 PerspectiveOffCenter(float left, float right, float bottom, float top, float near, float far)
+    {
+        float x = 2.0F * near / (right - left);
+        float y = 2.0F * near / (top - bottom);
+        float a = (right + left) / (right - left);
+        float b = (top + bottom) / (top - bottom);
+        float c = -(far + near) / (far - near);
+        float d = -(2.0F * far * near) / (far - near);
+        float e = -1.0F;
+        Matrix4x4 m = new Matrix4x4();
+        m[0, 0] = x;
+        m[0, 1] = 0;
+        m[0, 2] = a;
+        m[0, 3] = 0;
+        m[1, 0] = 0;
+        m[1, 1] = y;
+        m[1, 2] = b;
+        m[1, 3] = 0;
+        m[2, 0] = 0;
+        m[2, 1] = 0;
+        m[2, 2] = c;
+        m[2, 3] = d;
+        m[3, 0] = 0;
+        m[3, 1] = 0;
+        m[3, 2] = e;
+        m[3, 3] = 0;
+        return m;
+    }
+
+    public float Velocity = 0.5f;
+    public int tabSize = 100;
+    //
+    private Vector3 center = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+    private float intensity = 0;
+
+    void Update()
+    {
+
+        if (Input.GetMouseButton(0) == true)
+        {
+            Vector3 mouse = Input.mousePosition;
+            Vector3 dir = mouse - center;
+
+            dir.z = dir.y;
+            dir.y = 0;
+
+
+            //dir.Normalize();
+
+            if (Screen.width - tabSize < mouse.x)
+                intensity -= ((Screen.width - mouse.x) - tabSize);
+            if (tabSize > mouse.x)
+                intensity += (tabSize - mouse.x);
+            if (Screen.height - tabSize < mouse.y)
+                intensity -= ((Screen.height - mouse.y) - tabSize);
+            if (tabSize > mouse.y)
+                intensity += (tabSize - mouse.y);
+
+            intensity /= tabSize * 10;
+            transform.Translate(Velocity * dir * intensity * Time.deltaTime, Space.World);
+        }
+    }
+}
